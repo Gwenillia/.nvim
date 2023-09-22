20b3b7880649155c7da9f7cbd3476525a7cb084a
Gwen Tripet-Costet <gwenden31@gmail.com>
2023-09-22 15:29:53 +0200

    Clean code :wq

--- lua/gwenillia/packer.lua
+++ lua/gwenillia/packer.lua
@@ -76,7 +76,7 @@ return require('packer').startup(function(use)
   })
   use {
     "akinsho/toggleterm.nvim", tag = '*', config = function()
-     require("toggleterm").setup()
-   end
- }
+    require("toggleterm").setup()
+  end
+  }
 end)
