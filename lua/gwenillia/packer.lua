5eed1ed665b4bad74277d6b7dc0d91214d91df69
Gwen Tripet-Costet <gwenden31@gmail.com>
2023-10-02 16:36:31 +0200

    clean

--- lua/gwenillia/packer.lua
+++ lua/gwenillia/packer.lua
@@ -82,19 +82,7 @@ return require('packer').startup(function(use)
   use {
     "nvim-neorg/neorg",
     config = function()
-      require('neorg').setup {
-        load = {
-          ["core.defaults"] = {},        -- Loads default behaviour
-          ["core.concealer"] = {},       -- Adds pretty icons to your documents
-          ["core.dirman"] = {            -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-            },
-          },
-        },
-      }
+      require('neorg').setup {}
     end,
     run = ":Neorg sync-parsers",
     requires = "nvim-lua/plenary.nvim",
