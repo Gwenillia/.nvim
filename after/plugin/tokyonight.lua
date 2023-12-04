e676fed23a2cf9a365e26ce22c9c2eb02644fc0a
Gwen Tripet-Costet <gwenden31@gmail.com>
2023-12-04 09:28:14 +0100

    update tokyonight to match orange colors

--- after/plugin/tokyonight.lua
+++ after/plugin/tokyonight.lua
@@ -4,7 +4,17 @@ require("tokyonight").setup({
   styles = {
     sidebars = "transparent",
     floats = "transparent",
-  }
+  },
+  on_highlights = function(highlight)
+    highlight.LineNr = { ctermfg=216, fg='#FFA07A' }
+    highlight.PmenuSel = { ctermbg=166, ctermfg=230, bg='#FF7F50', fg='#fff8DC' }
+    highlight.GitSignsAdd = { ctermfg=46, fg='#9F8B89' }
+    highlight.GitSignsChange = { ctermfg=208, fg='#FFD700' }
+    highlight.GitSignsDelete = { ctermfg=196, fg='#FF6347' }
+    highlight.GitSignsAddNr = { ctermfg=46, fg='#9F8B89' }
+    highlight.GitSignsChangeNr = { ctermfg=208, fg='#FFD700' }
+    highlight.GitSignsDeleteNr = { ctermfg=196, fg='#FF6347' }
+  end,
 })
 
 require("tokyonight").colorscheme()
