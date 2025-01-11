c681bedb4ecf41e19d380b100325aa4596037d7e
Gwen Tripet-Costet <gwenden31@gmail.com>
2025-01-11 16:37:02 +0100

    add suggested defualt config lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,8 +1,22 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    component_separators = '|',
-    section_separators = '',
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
+    }
   },
   sections = {
     lualine_x = {
@@ -21,7 +35,18 @@ require('lualine').setup {
       {
         'buffers',
       }
-    }
-  }
+    },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
-
