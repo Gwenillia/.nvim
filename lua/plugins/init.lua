9b52cf11e93e13ab81efb4d07dd493d818efcb30
Gwenillia <gwenillia@icloud.com>
2025-12-10 17:09:33 +0100

    update telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,4 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', version = '0.1.3',
-    -- or                            , branch = '0.1.x',
-    dependencies = { { 'nvim-lua/plenary.nvim' } }
-  },
   --- TODO undo comments to use tokyonight
   -- {
     -- "folke/tokyonight.nvim",
